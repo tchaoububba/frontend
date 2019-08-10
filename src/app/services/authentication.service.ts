8618826dde44de710c51b3400b1391b915e6ec01
tchaoububba <tchaoububba@gmail.com>
2019-08-10 13:08:27 -0400

    Trying to pull

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -14,24 +14,14 @@ export class AuthenticationService {
       let body = JSON.parse(JSON.stringify(user));
       console.log(body);
       // Remember to change URL
-<<<<<<< HEAD
 
-      return this._httpService.post("http://desktop-tpu8n71:9009/user-ws/api/login", body);
-=======
       return this._httpService.post("http://desktop-osghs5h:9009/user-ws/api/login", body);
->>>>>>> 2882fe476853dabf62ec0aefe7ab92f62cf611ce
     }
 
     register(user: User) {
       console.log("Before enter register");
-<<<<<<< HEAD
-      return this._httpService.post("http://desktop-tpu8n71:9009/user-ws/api/user",user);
-
-    } 
-=======
       return this._httpService.post("http://desktop-osghs5h:9009/user-ws/api/user",user);
     }
->>>>>>> 2882fe476853dabf62ec0aefe7ab92f62cf611ce
   
     setConnectedUser(user) {
       this.connectedUser = user;
